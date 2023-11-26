c748539093864829ee8b541afe5bd96bdce99701
Benjamin Brassart <bbrassar@student.42.fr>
2023-11-26 04:58:49 +0100

    Remove tmux detection in fish Use skim as fuzzy finder

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,20 @@
 set -x XDG_CONFIG_HOME ~/.config
 
-if not set -q TMUX
-	exec tmux
-end
-
 functions -e fish_greeting
 
-fish_add_path ~/.yarn/bin
-fish_add_path ~/.cargo/bin
-fish_add_path ~/.local/bin
+fish_add_path -a ~/.{yarn,cargo,locl}/bin
 
 alias l 'eza --git-ignore --git --header --group --icons=auto'
 alias editor vim
 alias vi vim
 alias vim nvim
 
-set -x BAT_THEME "Catppuccin-mocha"
 set -x LS_COLORS (vivid generate catppuccin-mocha)
-set -x FZF_DEFAULT_OPTS "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-set -x MAKEFLAGS -j (lscpu --parse=CPU | grep -v '^#' | wc -l)
+set -x SKIM_DEFAULT_OPTIONS "--color=fg:#cdd6f4,bg:#1e1e2e,matched:#313244,matched_bg:#f2cdcd,current:#cdd6f4,current_bg:#45475a,current_match:#1e1e2e,current_match_bg:#f5e0dc,spinner:#a6e3a1,info:#cba6f7,prompt:#89b4fa,cursor:#f38ba8,selected:#eba0ac,header:#94e2d5,border:#6c7086"
+
+set CPU_COUNT (lscpu --parse=CPU | grep -v '^#' | wc -l)
+set -x MAKEFLAGS -j $CPU_COUNT
 
 if status --is-interactive
   set -x GPG_TTY (tty)
@@ -30,4 +25,5 @@ if status --is-interactive
   alias ll 'l -l'
   alias la 'l -a'
   alias lla 'l -la'
+  alias fzf sk
 end
