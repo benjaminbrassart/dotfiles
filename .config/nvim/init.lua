6f1cb542435b9280f9cf36cfa7b3e655df1cb145
Benjamin Brassart <bbrassar@student.42.fr>
2024-01-17 22:24:53 +0100

    Add some neovim lsp related config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -124,19 +124,18 @@ local lsps = {
 		},
 	},
 	clangd = {},
+	gopls = {},
+	rust_analyzer = {},
+	taplo = {},
 }
-
-local function on_lsp_attach()
-	vim.api.nvim_set_keymap("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true })
-end
 
 require("mason").setup {}
 require("mason-lspconfig").setup {
 	ensure_installed = lsps,
+	automatic_installation = true,
 }
 
 for lsp, config in pairs(lsps) do
-	config.on_attach = on_lsp_attach
 	require("lspconfig")[lsp].setup(config)
 end
 
@@ -162,5 +161,48 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 		vim.opt_local.tabstop = 4
 		vim.opt_local.softtabstop = 4
 		vim.opt_local.shiftwidth = 4
+	end,
+})
+
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+-- https://github.com/neovim/nvim-lspconfig#suggested-configuration
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = {
+			buffer = ev.buf,
+		}
+
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+
+		-- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+		-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		-- vim.keymap.set('n', '<space>wl', function()
+		-- 	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		-- end, opts)
+
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<space>f", function()
+			vim.lsp.buf.format {
+				async = true,
+			}
+		end, opts)
+		vim.keymap.set({ "n", "i" }, "<C-space>", vim.lsp.omnifunc, opts)
 	end,
 })
